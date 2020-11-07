ab5afa31f0afb1d9f67b49aeac3b70ba4f454cb7
Matt Choi <choim3@spu.edu>
2020-11-07 04:56:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
  
   name_hash.each do |item, price|
     
-    if min_value == nil || price < min_value
+    if  price < min_value
       min_key = item
       min_value = price
     end
